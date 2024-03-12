ca50a66d9082f301f90dbc8767e705dcfc371e37
Политехнический Колледж Городского Хозяйства <vs@pkgh.ru>
2024-03-12 23:07:15 +0300

    addcode

--- newproject.py
+++ newproject.py
@@ -1,1 +1,7 @@
-print("Gornak")
+def merge(dic1,dic2):
+    dic3=dic1.copy()
+    dic3.update(dic2)
+    return dic3
+dic1={1:"hello", 2:"world"}
+dic2={3:"Python", 4:"Programming"}
+print (merge(dic1,dic2)) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
