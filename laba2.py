b1f2da64788e326998b721ed27c312b406e9980d
Политехнический Колледж Городского Хозяйства <vs@pkgh.ru>
2024-03-12 23:25:46 +0300

    lab01py

--- laba2.py
+++ laba2.py
@@ -3,11 +3,11 @@ import random
 def generate_random_list(num_elements):
     journal_number = 7
     min_range = 5
-    max_range = journal_number * 100 + 10
+    max_range = journal_number * 100
     random_list = [random.randint(min_range, max_range) for _ in range(num_elements)]
     return random_list
 
 # Пример вызова функции с указанным числом элементов
-num_elements = 20
+num_elements = 17
 random_list = generate_random_list(num_elements)
 print(random_list)
